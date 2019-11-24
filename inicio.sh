feea6278967a9d7f2645f80bf20399c1b94a1eab
Joaquin Madrid Belando <ea5gvk@gmail.com>
2019-11-24 20:35:57 +0100

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 rpi-rw
 sudo -s
-rpi-rw
 git clone https://github.com/ea5gvk/ysfxlxvk4tux.git
 cd ysfxlxvk4tux
 cp ysfxlx.sh /usr/local/sbin/ysfxlx.sh
 chmod 755 /usr/local/sbin/ysfxlx.sh
-sudo ./usr/local/sbin/ysfxlx.sh
+./usr/local/sbin/ysfxlx.sh
 cp pistar-daily /etc/cron.daily/pistar-daily
 chmod 755 /etc/cron.daily/pistar-daily
 cd ..
