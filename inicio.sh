32759a96ab1cfb40f191a8b92ca929901c1acea7
Joaquin Madrid Belando <ea5gvk@gmail.com>
2019-11-24 20:29:08 +0100

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -2,7 +2,7 @@
 rpi-rw
 sudo -s
 rpi-rw
-wget https://github.com/ea5gvk/ysfxlxvk4tux.git
+git clone https://github.com/ea5gvk/ysfxlxvk4tux.git
 cd ysfxlxvk4tux
 cp ysfxlx.sh /usr/local/sbin/ysfxlx.sh
 chmod 755 /usr/local/sbin/ysfxlx.sh
